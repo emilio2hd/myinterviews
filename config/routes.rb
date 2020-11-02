01389f677b3052100f9acdae4a6e5001cd767e77
Emilio <emilio2hd@gmail.com>
2020-11-02 19:52:40 +0000

    Change the root pointer to static controller and move api under the "/api" scope.

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'static#index'
+  
+  scope '/api' do
+    defaults format: :json do
+      get '/dashboard', to: 'home#index'
+    end
 
-  defaults format: :json do
-    get '/dashboard', to: 'home#index'
-  end
+    resources :my_applications
+    resources :interviews
 
-  resources :my_applications
-  resources :interviews
+    resources :settings, only: [:index] do
+      collection do
+        patch :update_all
+      end
+    end
 
-  resources :settings, only: [:index] do
-    collection do
-      patch :update_all
+    resources :cover_letters do
+      member do
+        get :duplicate
+        get :new_email
+        post :send_email
+      end
     end
   end
 
-  resources :cover_letters do
-    member do
-      get :duplicate
-      get :new_email
-      post :send_email
-    end
-  end
+  get '*other', to: 'static#index'
 end
