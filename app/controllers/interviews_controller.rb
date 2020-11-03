85827c61876b1dd2ac029a8cf5df1fb25f64ada3
Emilio <emilio2hd@gmail.com>
2020-11-03 18:29:40 +0000

    Remove interviews rails views

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,21 +1,13 @@
 class InterviewsController < ApplicationController
-  before_action :set_interview, only: %i[show edit update destroy]
-  before_action :load_applications, only: %i[new edit]
+  before_action :set_interview, only: %i[show update destroy]
 
   def index
     @interviews = Interview.includes(:my_application).ordered_by_last.page(params[:page])
-    respond_to :html, :json
+    respond_to :json
   end
 
   def show
-    respond_to :html, :json
-  end
-
-  def new
-    @interview = Interview.new(my_application_id: params[:my_application_id])
-  end
-
-  def edit
+    respond_to :json
   end
 
   def create
@@ -23,13 +15,8 @@ class InterviewsController < ApplicationController
 
     respond_to do |format|
       if @interview.save
-        format.html { redirect_to @interview, notice: t('messages.successfully_created', entity: t('interviews.item')) }
         format.json { render json: @interview, status: :ok }
       else
-        format.html {
-          load_applications
-          render :new 
-        }
         format.json { render json: @cover_letter, status: :bad_request }
       end
     end
@@ -38,13 +25,8 @@ class InterviewsController < ApplicationController
   def update
     respond_to do |format|
       if @interview.update(interview_params)
-        format.html { redirect_to @interview, notice: t('messages.successfully_updated', entity: t('interviews.item')) }
         format.json { render json: @interview, status: :ok }
       else
-        format.html {
-          load_applications
-          render :edit 
-        }
         format.json { render json: @cover_letter, status: :bad_request }
       end
     end
@@ -54,7 +36,6 @@ class InterviewsController < ApplicationController
     @interview.destroy
 
     respond_to do |format|
-      format.html { redirect_to interviews_path, notice: t('messages.successfully_destroyed', entity: t('interviews.item')) }
       format.json { head :ok }
     end
   end
@@ -66,10 +47,6 @@ class InterviewsController < ApplicationController
   end
 
   private
-
-  def load_applications
-    @applications = MyApplication.all
-  end
 
   def set_interview
     @interview = Interview.includes(:my_application).find(params[:id])
