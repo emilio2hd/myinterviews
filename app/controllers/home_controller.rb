21a7b240a0556ea338e06afeffec24f67caab34a
Emilio <emilio2hd@gmail.com>
2020-11-03 17:59:51 +0000

    Remove home rails views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @applications = MyApplication.last_10.all
     @interviews   = Interview.includes(:my_application).next_10.all
-    respond_to :html, :json
+    respond_to :json
   end
 end
