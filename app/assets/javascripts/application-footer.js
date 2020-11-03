c8d742f66c84ea4561482e18cc6ce305c75af21c
Emilio <emilio2hd@gmail.com>
2020-11-03 22:48:40 +0000

    Remove adminlte library

--- app/assets/javascripts/application-footer.js
+++
@@ -1,7 +0,0 @@
-//= require bootstrap/js/bootstrap
-//= require plugins/slimScroll/jquery.slimscroll.min
-//= require plugins/fastclick/fastclick
-//= require plugins/select2/select2.full.min
-//= require plugins/bootstrap-datetimepicker-4.17.47/js/moment
-//= require plugins/bootstrap-datetimepicker-4.17.47/js/bootstrap-datetimepicker.min
-//= require dist/js/app
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,13 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-# Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'adminlte-2.3.6')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'adminlte-2.3.6', 'bootstrap', 'fonts')
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[application-footer.js]
-Rails.application.config.assets.precompile += %w[*.svg *.eot *.woff *.woff2 *.ttf]
 
 Rails.application.config.tinymce.install = :copy
