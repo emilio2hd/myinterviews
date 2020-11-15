f395fc16e0ef85270616d680f99f99b6c16ba39a
Emilio <emilio2hd@gmail.com>
2020-11-15 19:58:25 +0000

    Remove unused cover letter resolver

--- frontend/projects/myinterviews/src/app/modules/cover-letter/cover-letter.module.ts
+++ frontend/projects/myinterviews/src/app/modules/cover-letter/cover-letter.module.ts
@@ -9,7 +9,6 @@ import { DemoNgZorroAntdModule } from '../../ng-zorro-antd.module';
 import { CoverLetterRoutingModule } from './cover-letter-routing.module';
 import { CoverLetterListComponent } from './list/list.component';
 import { CoverLetterService } from './cover-letter.service';
-import { CoverLetterResolver } from './cover-letter.resolver';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { EditorDrawerComponent } from './editor-drawer/editor-drawer.component';
 import { EditorDrawerTitleComponent } from './editor-drawer-title/editor-drawer-title.component';
@@ -30,6 +29,5 @@ import { EmailSenderComponent } from './email-sender/email-sender.component';
     EditorDrawerTitleComponent,
     EmailSenderComponent,
   ],
-  providers: [CoverLetterService, CoverLetterResolver],
 })
 export class CoverLetterModule {}
