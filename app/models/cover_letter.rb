78c6c3237606fab7a0fa8122252ae3ab070785b0
Emilio <emilio2hd@gmail.com>
2020-11-16 19:10:56 +0000

    Remove cover letter lock_version field

--- app/models/cover_letter.rb
+++ app/models/cover_letter.rb
@@ -4,7 +4,6 @@
 #
 #  id           :integer          not null, primary key
 #  content      :text
-#  lock_version :integer          default(0), not null
 #  title        :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200419212833) do
+ActiveRecord::Schema.define(version: 20201116181717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20200419212833) do
   create_table "cover_letters", id: :serial, force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "lock_version", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
