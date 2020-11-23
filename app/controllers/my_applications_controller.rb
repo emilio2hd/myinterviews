177c5bda5d30c443b98a009ed4862db82b5eacc9
Emilio <emilio2hd@gmail.com>
2020-11-23 18:56:05 +0000

    Update my applications controller to inherits from api controller

--- app/controllers/my_applications_controller.rb
+++ app/controllers/my_applications_controller.rb
@@ -1,46 +1,37 @@
-class MyApplicationsController < ApplicationController
+class MyApplicationsController < ApiController
   before_action :set_my_application, only: %i[show update destroy]
 
   def index
     @my_applications = MyApplication.ordered_by_last.page(params[:page])
-    respond_to :json
   end
 
   def show
     @interviews = Interview.where(my_application_id: @my_application.id)
                            .ordered_by_last.group_by { |i| localize(i.at, format: :only_date) }
-    
-    respond_to :json
   end
 
   def create
     @my_application = MyApplication.new(my_application_params)
 
-    respond_to do |format|
-      if @my_application.save
-        format.json { render json: @my_application, status: :ok }
-      else
-        format.json { render json: @my_application, status: :bad_request }
-      end
+    if @my_application.save
+      render json: @my_application
+    else
+      render json: @my_application, status: :bad_request
     end
   end
 
   def update
-    respond_to do |format|
-      if @my_application.update(my_application_params)
-        format.json { render json: @my_application, status: :ok }
-      else
-        format.json { render json: @my_application, status: :bad_request }
-      end
+    if @my_application.update(my_application_params)
+      render json: @my_application
+    else
+      render json: @my_application, status: :bad_request
     end
   end
 
   def destroy
     @my_application.destroy
 
-    respond_to do |format|
-      format.json { head :ok }
-    end
+    head :ok
   end
 
   private
