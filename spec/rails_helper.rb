ebbcacc3a783edd3d71f58cefbd23524f078ec5c
Emilio <emilio2hd@gmail.com>
2020-11-24 08:35:39 +0000

    Run coverage only when specified by environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 
-require 'simplecov'
-SimpleCov.start 'rails'
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start :rails
+end
 
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
