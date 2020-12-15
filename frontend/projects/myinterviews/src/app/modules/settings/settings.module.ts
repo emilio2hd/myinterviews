b74c831fe12c6234a4aef3e1457a649f7532870a
Emilio <emilio2hd@gmail.com>
2020-12-15 23:23:15 +0000

    Refactoring settings components to use single layout component

--- frontend/projects/myinterviews/src/app/modules/settings/settings.module.ts
+++ frontend/projects/myinterviews/src/app/modules/settings/settings.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { LayoutModule } from '@app/@layout/layout.module';
+
 import { DemoNgZorroAntdModule } from '../../ng-zorro-antd.module';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsFormComponent } from './form/form.component';
@@ -9,7 +11,13 @@ import { SettingsResolver } from './settings.resolver';
 
 @NgModule({
   declarations: [SettingsFormComponent],
-  imports: [CommonModule, SettingsRoutingModule, ReactiveFormsModule, DemoNgZorroAntdModule],
+  imports: [
+    CommonModule,
+    SettingsRoutingModule,
+    LayoutModule,
+    ReactiveFormsModule,
+    DemoNgZorroAntdModule,
+  ],
   providers: [SettingsResolver],
 })
 export class SettingsModule {}
