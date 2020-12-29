4425b50ceb415929e3cbc27935c648bd59dde73b
Emilio <emilio2hd@gmail.com>
2020-12-29 16:18:49 +0000

    Refactoring cover letter list page

--- frontend/projects/myinterviews/src/app/modules/cover-letter/pages/cover-letter-page.component.ts
+++ frontend/projects/myinterviews/src/app/modules/cover-letter/pages/cover-letter-page.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { BehaviorSubject, pipe } from 'rxjs';
+import { Component, OnInit, OnDestroy, TemplateRef, ViewChild } from '@angular/core';
+import { BehaviorSubject, pipe, Subscription } from 'rxjs';
 import { finalize, switchMap, tap } from 'rxjs/operators';
 
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { NzTableQueryParams } from 'ng-zorro-antd/table';
 import { NzDrawerRef, NzDrawerService } from 'ng-zorro-antd/drawer';
 
 import { CoverLetter } from '@core/models';
@@ -17,26 +16,29 @@ import { EditorDrawerComponent, EmailSenderComponent } from '../components';
   templateUrl: './cover-letter-page.component.html',
   styleUrls: ['./cover-letter-page.component.scss'],
 })
-export class CoverLetterPageComponent implements OnInit {
-  private paginationParamsSubject = new BehaviorSubject<{ pageIndex: number }>({
-    pageIndex: 1,
-  });
-  private paginationParams$ = this.paginationParamsSubject.asObservable();
+export class CoverLetterPageComponent implements OnInit, OnDestroy {
+  private subscriptions = new Subscription();
+  private paginationParams$ = new BehaviorSubject<number>(1);
   private getAllCoverLetters$ = pipe(
     tap(() => (this.loading = true)),
-    switchMap(({ pageIndex }) =>
-      this.coverLetterService
-        .getPaginatedResults(pageIndex)
-        .pipe(finalize(() => (this.loading = false)))
+    switchMap((page: number) =>
+      this.coverLetterService.getPaginatedResults(page).pipe(finalize(() => (this.loading = false)))
     )
   );
 
   @ViewChild('drawerTitle', { static: false }) drawerTitle?: TemplateRef<{}>;
 
-  loading = true;
-  coverLetter$ = this.getAllCoverLetters$(this.paginationParams$);
+  coverLetter$ = this.getAllCoverLetters$(this.paginationParams$.asObservable());
   coverLetterOnFocus: CoverLetter;
   drawerRef: NzDrawerRef<EditorDrawerComponent, { coverLetter?: CoverLetter; refresh: boolean }>;
+
+  total: number;
+  coverLetters: CoverLetter[];
+  pageSize: number;
+  pageIndex: number;
+  loading: boolean;
+
+  trackById = (_: number, item: CoverLetter) => item.id;
 
   constructor(
     private coverLetterService: CoverLetterApiService,
@@ -45,7 +47,16 @@ export class CoverLetterPageComponent implements OnInit {
     private modalService: NzModalService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.coverLetter$.subscribe((paginatedResult) => {
+        this.total = paginatedResult.totalCount;
+        this.pageSize = paginatedResult.pageSize;
+        this.pageIndex = paginatedResult.currentPage;
+        this.coverLetters = paginatedResult.data;
+      })
+    );
+  }
 
   onNewClick() {
     this.onEditClick({} as CoverLetter);
@@ -66,17 +77,19 @@ export class CoverLetterPageComponent implements OnInit {
 
     this.coverLetterOnFocus = coverLetter;
 
-    this.drawerRef.afterClose.subscribe((closeResult = { refresh: false }) => {
-      this.coverLetterOnFocus = undefined;
+    this.subscriptions.add(
+      this.drawerRef.afterClose.subscribe((closeResult = { refresh: false }) => {
+        this.coverLetterOnFocus = undefined;
 
-      if (closeResult.refresh) {
-        this.paginationParamsSubject.next({ pageIndex: 1 });
-      }
-    });
+        if (closeResult.refresh) {
+          this.paginationParams$.next(1);
+        }
+      })
+    );
   }
 
-  onQueryParamsChange(params: NzTableQueryParams): void {
-    this.paginationParamsSubject.next(params);
+  onPageIndexChange(nextPage: number) {
+    this.paginationParams$.next(nextPage);
   }
 
   onSendClick(coverLetter: CoverLetter): void {
@@ -94,17 +107,28 @@ export class CoverLetterPageComponent implements OnInit {
   }
 
   onDeleteConfirm(coverLetter: CoverLetter) {
-    this.coverLetterService.delete(coverLetter.id).subscribe({
-      next: () => {
-        const currentParams = this.paginationParamsSubject.value;
-        this.paginationParamsSubject.next({ ...currentParams });
+    this.subscriptions.add(
+      this.coverLetterService.delete(coverLetter.id).subscribe({
+        next: () => {
+          this.paginationParams$.next(1);
 
-        this.notification.success('Cover Letter', `"${coverLetter.title}" successfully deleted`);
+          this.notification.success('Cover Letter', `"${coverLetter.title}" successfully deleted`);
 
-        if (this.drawerRef) {
-          this.drawerRef.close({ refresh: true });
-        }
-      },
-    });
+          if (this.drawerRef) {
+            this.drawerRef.close({ refresh: true });
+          }
+        },
+        error: () => {
+          this.notification.error(
+            'Cover Letter',
+            `Uh-oh! Something wrong has happened. Unable to delete "${coverLetter.title}"`
+          );
+        },
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
