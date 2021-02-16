72abad4289bc476166dc317f1ef63bf4570e990b
Emilio <emilio2hd@gmail.com>
2021-02-16 19:43:09 +0000

    Remove view helpers not used anymore

--- app/helpers/interviews_helper.rb
+++
@@ -1,8 +0,0 @@
-module InterviewsHelper
-  ICON = { talk: { icon: 'fa-comments bg-yellow', title: I18n.t('activerecord.attributes.interview.type_ofs.talk') },
-           technical: { icon: 'fa-wrench bg-aqua', title: I18n.t('activerecord.attributes.interview.type_ofs.technical') } }.freeze
-
-  def interview_icon(enum_type)
-    content_tag(:i, '', class: "fa #{ICON[enum_type.to_s.to_sym][:icon]}", title: ICON[enum_type.to_s.to_sym][:title])
-  end
-end
