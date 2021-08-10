1b9fcc8b4232901f8c413ed8f5dcdeec5f129c0d
Emilio <emilio2hd@gmail.com>
2021-08-10 01:22:01 +0100

    Update rubocop to v1.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,8 +55,8 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = :random
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
@@ -81,10 +81,10 @@ RSpec.configure do |config|
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
   # if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    # config.default_formatter = 'doc'
+  #  # Use the documentation formatter for detailed output,
+  #  # unless a formatter has already been configured
+  #  # (e.g. via a command-line flag).
+  #  # config.default_formatter = 'doc'
   # end
 
   # Print the 10 slowest examples and example groups at the
